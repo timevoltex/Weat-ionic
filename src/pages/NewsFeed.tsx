4a9b9fc882b431d4f0d15b99aabe6e6a58841a32
timevoltex <buja801@daum.net>
2020-04-14 23:21:55 +0900

    20.04.14 change layout
    Change layout following designer lafp's new weat page(Xd)

--- src/pages/NewsFeed.tsx
+++ src/pages/NewsFeed.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHashtag } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
@@ -11,10 +11,19 @@ import {
   IonIcon,
   IonButton,
   IonList,
+  IonTitle,
+  IonSearchbar,
+  IonAvatar,
+  IonLabel,
+  IonItem,
+  IonSlides,
+  IonSlide,
+  IonChip,
 } from "@ionic/react";
 import { RouteComponentProps } from "react-router";
-import { location } from "ionicons/icons";
+import { personCircle, heart, chatboxEllipses, bookmark } from "ionicons/icons";
 import Detail from './MenuDetail';
+
 type List = {
   src: string;
   name: string;
@@ -62,23 +71,91 @@ const items: List[] = [
 const Page = styled(IonPage)`
   padding: 10px;
 `;
-const NewsFeed: React.FC<RouteComponentProps> = ({history}) => {
+
+const Feed = styled(IonText)`
+  p{overflow:hidden;
+  text-overflow:ellipsis;
+  white-space:nowrap;
+  width:60vw;}
+`
+const NewsFeed: React.FC<RouteComponentProps> = ({ history }) => {
+  const [searchText, setSearchText] = useState('');
   return (
     <Page>
       <IonHeader>
-        <IonToolbar>
-          <FontAwesomeIcon icon={faHashtag} style={{ color: "#8217FF" }} />
-          <IonText slot="end" style={{ fontSize: "10px" }}>
-            강원도 춘천시 효자동
-            <IonIcon
-              icon={location}
-              style={{ fontSize: "14px", color: "#8217FF" }}
-            />
+        <IonToolbar style={{ height: "50px" }}>
+          <IonTitle style={{ textAlign: 'center' }}>
+            <img src="assets/img/weat-purple.png" alt="logo" style={{ width: '50px' }} />
+          </IonTitle>
+          <IonText slot="start">
+            <img src="assets/icon/locationg-01.png" alt="loc" style={{ width: "50px" }} />
+          </IonText>
+          <IonText slot="end">
+            <img src="assets/icon/lists-01.png" alt="lists" style={{ width: "50px" }} />
           </IonText>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-      <IonList>
+        <IonSearchbar value={searchText} placeholder="어디가 맛있지?" onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
+        <div className="feedContainer">
+          <IonItem lines="none">
+            <IonAvatar slot="start">
+              <IonIcon icon={personCircle} style={{ fontSize: '50px' }} />
+            </IonAvatar>
+            <IonLabel position="stacked">
+              <p style={{ marginBottom: 0 }}>유저명</p>
+              <p style={{ marginTop: 0 }}>33% 00:00</p>
+            </IonLabel>
+            <IonLabel position="stacked" slot="end" style={{ textAlign: 'right', marginTop: "10px" }}>
+              <p style={{ marginBottom: 0 }}>마리스 커피</p>
+              <p style={{ marginTop: 0 }}>아이스 아메리카노</p>
+            </IonLabel>
+          </IonItem>
+          <IonSlides options={{ slidesPerView: 2.5, freeMode: 'true', spaceBetween: 20 }}>
+            {items.map((image, i) => (
+              <IonSlide key={i}>
+                <img src={image.src} alt="img" />
+              </IonSlide>
+            ))}
+          </IonSlides>
+          <IonItem lines="none" style={{ marginTop: '10px' }}>
+              <IonIcon icon={heart} style={{ fontSize: '29px' }} />
+              <IonIcon icon={chatboxEllipses} style={{ fontSize: '29px' }} />
+              <IonIcon icon={bookmark} style={{ fontSize: '29px' }} />
+            <IonAvatar>
+            </IonAvatar>
+            <IonAvatar>
+            </IonAvatar>
+            <IonAvatar>
+            </IonAvatar>
+            <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', width: '43vw', marginLeft: 'auto', marginTop: '-5px' }}>
+              <IonChip style={{ fontSize: '8px' }}>해시태그</IonChip>
+              <IonChip style={{ fontSize: '8px' }}>해시태그</IonChip>
+              <IonChip style={{ fontSize: '8px' }}>해시태그</IonChip>
+            </div>
+          </IonItem>
+          <div className="feedzone">
+            <Feed>
+              <p>
+                <span style={{ fontWeight: 'bold' }}>lorem </span>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, ullam? Ea quaerat sit tempore quam error, architecto laudantium quia beatae nisi, rem ut atque totam saepe voluptatem voluptatum culpa consequatur.</p>
+            </Feed>
+            <Feed>
+              <p>
+                <span style={{ fontWeight: 'bold' }}>lorem </span>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </Feed>
+            <Feed>
+              <p>
+                <span style={{ fontWeight: 'bold' }}>lorem </span>
+                Lorem ipsum dolor sit amet,
+                </p>
+              <IonIcon icon={heart} slot="end" />
+            </Feed>
+          </div>
+        </div>
+
+        {/* <IonList>
       {items.map((image, i) => (
         <IonButton className={'navImage'} key={i * 10} fill='clear'
         onClick={(e) => {
@@ -91,10 +168,10 @@ const NewsFeed: React.FC<RouteComponentProps> = ({history}) => {
             alt="test"
             style={{ margin: 0 }}
           />
-          {/* </IonRouterLink> */}
+          {/* </IonRouterLink>
         </IonButton>
       ))}
-    </IonList>
+    </IonList> */}
       </IonContent>
     </Page>
   );
