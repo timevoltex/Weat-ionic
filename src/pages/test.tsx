fe644464726a584952e852a355f5e1f102b2b33f
yeoneum <52909529+timevoltex@users.noreply.github.com>
2020-04-17 17:43:29 +0900

    20.04.17 테스트 추가
    세그먼트 레이아웃을 확인하기 위해 예제 복사

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar, IonFooter } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar, IonFooter, IonSegment, IonSegmentButton, IonLabel, IonIcon } from '@ionic/react';
+import { camera, bookmark, basket, globe, call, star, pin, heart, home } from 'ionicons/icons';
 
 const SearchBarExamples: React.FC = () => {
   const [searchText, setSearchText] = useState('');
@@ -52,6 +53,95 @@ const SearchBarExamples: React.FC = () => {
           <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
         </IonToolbar>
 
+
+        <IonSegment onIonChange={e => console.log('Segment selected', e.detail.value)}>
+          <IonSegmentButton value="friends">
+            <IonLabel>Friends</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="enemies">
+            <IonLabel>Enemies</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
+
+        {/*-- Disabled Segment --*/}
+        <IonSegment onIonChange={e => console.log('Segment selected', e.detail.value)} disabled value="sunny">
+          <IonSegmentButton value="sunny">
+            <IonLabel>Sunny</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="rainy">
+            <IonLabel>Rainy</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
+
+        {/*-- Segment with anchors --*/}
+        <IonSegment onIonChange={e => console.log('Segment selected', e.detail.value)}>
+          <IonSegmentButton value="dogs">
+            <IonLabel>Dogs</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="cats">
+            <IonLabel>Cats</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
+
+        {/*-- Scrollable Segment --*/}
+        <IonSegment scrollable value="heart">
+          <IonSegmentButton value="home">
+            <IonIcon icon={home} />
+          </IonSegmentButton>
+          <IonSegmentButton value="heart">
+            <IonIcon icon={heart} />
+          </IonSegmentButton>
+          <IonSegmentButton value="pin">
+            <IonIcon icon={pin} />
+          </IonSegmentButton>
+          <IonSegmentButton value="star">
+            <IonIcon icon={star} />
+          </IonSegmentButton>
+          <IonSegmentButton value="call">
+            <IonIcon icon={call} />
+          </IonSegmentButton>
+          <IonSegmentButton value="globe">
+            <IonIcon icon={globe} />
+          </IonSegmentButton>
+          <IonSegmentButton value="basket">
+            <IonIcon icon={basket} />
+          </IonSegmentButton>
+        </IonSegment>
+
+        {/*-- Segment with secondary color --*/}
+        <IonSegment onIonChange={e => console.log('Segment selected', e.detail.value)} color="secondary">
+          <IonSegmentButton value="standard">
+            <IonLabel>Standard</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="hybrid">
+            <IonLabel>Hybrid</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="sat">
+            <IonLabel>Satellite</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
+
+        {/*-- Segment in a toolbar --*/}
+        <IonToolbar>
+          <IonSegment onIonChange={e => console.log('Segment selected', e.detail.value)}>
+            <IonSegmentButton value="camera">
+              <IonIcon icon={camera} />
+            </IonSegmentButton>
+            <IonSegmentButton value="bookmark">
+              <IonIcon icon={bookmark} />
+            </IonSegmentButton>
+          </IonSegment>
+        </IonToolbar>
+
+        {/*-- Segment with default selection --*/}
+        <IonSegment onIonChange={e => console.log('Segment selected', e.detail.value)} value="javascript">
+          <IonSegmentButton value="python">
+            <IonLabel>Python</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="javascript">
+            <IonLabel>Javascript</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
       </IonContent>
       <IonFooter>
         <IonToolbar>
